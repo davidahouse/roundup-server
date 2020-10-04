1086fd9b50e422b0ca93f1ac78f8a238ada0812c
David House <davidahouse@gmail.com>
2020-10-03 20:02:23 -0400

    :bug: fixed missing tests folder

---
+++ test/roundup-server-tests.js
@@ -0,0 +1,11 @@
+"use strict";
+/* eslint-env mocha */
+const expect = require("chai").expect;
+
+describe("Roundup Server", function () {
+  beforeEach(() => {});
+
+  it("should be tested", function () {
+    expect(true).to.equal(true);
+  });
+});
